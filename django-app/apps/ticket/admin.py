199e5f6ee688080fd128865e19967efdd3a5885e
Maciej Januszewski <maciek@mjanuszewski.pl>
2018-12-09 13:34:53 +0100

    Registered ticket adm~in

--- django-app/apps/ticket/admin.py
+++ django-app/apps/ticket/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.ticket.models import Ticket
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    """
+    Ticket Admin interface.
+    """
+    
+    pass
